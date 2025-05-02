4e525bcde971f50044bb3c897e2842b124b4035d
Dennis <d.shkreben@web.de>
2025-05-02 08:13:43 +0200

    Korrektur task 2

--- src/worksheet1/task2/Mail.java
+++ src/worksheet1/task2/Mail.java
@@ -4,14 +4,14 @@ public class Mail {
     private final String sender;
     private final String subject;
     private final String message;
-    private final String datetime;
+    private final Datetime datetime;
     private boolean read;
 
     public Mail(String sender, String subject, String message, String datetime) {
         this.sender = sender;
         this.subject = subject;
         this.message = message;
-        this.datetime = datetime;
+        this.datetime.now();
         read = false;
     }
 
