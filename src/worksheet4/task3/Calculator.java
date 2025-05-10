819f25e3a8771fb728b228a0b30793c443288668
Dennis <dshkrebe@smail.uni-koeln.de>
2025-05-10 14:29:54 +0200

    task3

---
+++ src/worksheet4/task3/Calculator.java
@@ -0,0 +1,44 @@
+package worksheet4.task3;
+
+public class Calculator {
+    public static double divide1(double a, double b) {
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            return 0;
+        }
+    }
+
+    public static double divide2(double a, double b) throws ArithmeticException {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        // Exception handling in method
+        double d1 = Calculator.divide1(3, 0);
+        System.out.println(d1);
+
+        // Delegated Exception Handling
+        try {
+            double d2 = Calculator.divide2(3, 0);
+            System.out.println(d2);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+        /* 3/0 wirft keine Exception, sondern es wird infinity ausgegeben???
+         * Wie ist das möglich?
+         */
+        /*
+         * Variante 2 ist sinnvoller, da aus der Methode entweder eine Zahl oder die
+         * Exception gegeben wird. Dies lässt sich in der aufzurufenden Methode
+         * unterscheiden.
+         * Bei Variante 1 muss die Methode trotzdem eine Zahl zurückgeben. Da dividieren
+         * alle Zahlen als Ergebnis haben kann, gibt es keine eindeutige Möglichkeit zu
+         * zeigen, dass die Division nicht geht.
+         */
+        /*
+         * Beim Berechnen einer Wurzel wäre Variante 1 sinnvoller, da -1 bei falscher
+         * Eingabe zurückgegeben werden kann, was dann auf einen Fehler verweist.
+         */
+    }
+}
