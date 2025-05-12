df14a753dd5b54f0984ff6c91a33b9fd5223b4a8
dennis <dshkrebe@smail.uni-koeln.de>
2025-05-12 13:27:45 +0200

    task4

--- src/worksheet4/task4/CheckoutService.java
+++ src/worksheet4/task4/CheckoutService.java
@@ -1,11 +1,13 @@
 package worksheet4.task4;
 
 public class CheckoutService {
-
+    //SRP: eine Klasse für Bezahlungen, eine für process Payment
+    //Interface: Keine konkreten Elemente, abstraktes Element
     public void processPayment(PaymentProvider p,double amount){
+        //Liskov: Es wird mit abstrakter Klasse PaymentProvider p gearbeitet
         p.pay(amount);
     }
-
+    //Open closed: Neue Klassen implementieren das Interface
     public static void main(String[] args){
         PaymentProvider p = new CreditCardPayment();
         PaymentProvider p2 = new PayPalPayment();
