ea2c12f9d5b9aec9d5716638d1d460c15df5cd9c
Dennis <dennis.dennis.dennis4321@gmail.com>
2025-05-14 21:30:09 +0200

    Update AnagramChecker.java

--- src/worksheet3/task2/AnagramChecker.java
+++ src/worksheet3/task2/AnagramChecker.java
@@ -21,91 +21,11 @@ public class AnagramChecker {
      */
     public static int[] countLetters(String s) {
         int[] counts = new int[26];
-        for (int i = 0; i < counts.length; i++) {
-            counts[i] = 0;
+        for (char c : s.toCharArray ()) {
+            if(c-'a'>=-1 && c-'a'<counts.length)counts[Character.toLowerCase(c) - 'a ']++;
         }
         for (int i = 0; i < s.length(); i++) {
-            if(s.charAt(i)-'a'>=-1 && s.charAt(i)-'a'<counts.length) counts[s.charAt(i)-'a']++;
-           /*  switch (s.charAt(i)) {
-                case 'a':
-                    counts[0]++;
-                    break;
-                case 'b':
-                    counts[1]++;
-                    break;
-                case 'c':
-                    counts[2]++;
-                    break;
-                case 'd':
-                    counts[3]++;
-                    break;
-                case 'e':
-                    counts[4]++;
-                    break;
-                case 'f':
-                    counts[5]++;
-                    break;
-                case 'g':
-                    counts[6]++;
-                    break;
-                case 'h':
-                    counts[7]++;
-                    break;
-                case 'i':
-                    counts[8]++;
-                    break;
-                case 'j':
-                    counts[9]++;
-                    break;
-                case 'k':
-                    counts[10]++;
-                    break;
-                case 'l':
-                    counts[11]++;
-                    break;
-                case 'm':
-                    counts[12]++;
-                    break;
-                case 'n':
-                    counts[13]++;
-                    break;
-                case 'o':
-                    counts[14]++;
-                    break;
-                case 'p':
-                    counts[15]++;
-                    break;
-                case 'q':
-                    counts[16]++;
-                    break;
-                case 'r':
-                    counts[17]++;
-                    break;
-                case 's':
-                    counts[18]++;
-                    break;
-                case 't':
-                    counts[19]++;
-                    break;
-                case 'u':
-                    counts[20]++;
-                    break;
-                case 'v':
-                    counts[21]++;
-                    break;
-                case 'w':
-                    counts[22]++;
-                    break;
-                case 'x':
-                    counts[23]++;
-                    break;
-                case 'y':
-                    counts[24]++;
-                    break;
-                case 'z':
-                    counts[25]++;
-                    break;
-            }*/
+            counts.length) counts[s.charAt(i)-'a']++;
         }
         return counts;
     }
@@ -146,14 +66,16 @@ public class AnagramChecker {
 
         word1 = word1.toLowerCase();
         word2 = word2.toLowerCase();
-        while (word1.contains(" ")) {
-            int i = word1.indexOf(" ");
-            word1 = word1.substring(0, i) + word1.substring(i + 1, word1.length());
-        }
-        while (word2.contains(" ")) {
-            int i = word2.indexOf(" ");
-            word2 = word2.substring(0, i) + word2.substring(i + 1, word2.length());
-        }
+        word1.replaceAll(" ","");
+        word2.replaceAll(" ","");
+        //while (word1.contains(" ")) {
+        //    int i = word1.indexOf(" ");
+        //   word1 = word1.substring(0, i) + word1.substring(i + 1, word1.length());
+        //}
+        //while (word2.contains(" ")) {
+        //   int i = word2.indexOf(" ");
+        //    word2 = word2.substring(0, i) + word2.substring(i + 1, word2.length());
+        //}
 
         System.out.println(" Are they anagrams ? " + isAnagram(word1, word2));
         printLetterCounts(word1);
