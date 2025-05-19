ceeef24ab32458231995af4ca52519fc6cd9cae7
Dennis <dshkrebe@smail.uni-koeln.de>
2025-05-19 19:25:50 +0200

    task3

--- Tests/TestFraction.java
+++ Tests/TestFraction.java
@@ -1,4 +1,14 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import worksheet5.task3.Fraction;
-public class TestFraction {
 
+public class TestFraction {
+    private Fraction f1;
+    private Fraction f2;
+
+    @BeforeEach
+    public void initialize(){
+        f1=new Fraction(3,4);
+        f2=new Fraction(1,2);
+    }
 }
